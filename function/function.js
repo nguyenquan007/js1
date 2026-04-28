833ac90e4e5f4f6bd0b15b96a3d6de66880ec05a
nguyenquan007 <tongchihieu.gsa@gmail.com>
2026-04-28 15:54:55 +0700

    function end

--- function/function.js
+++ function/function.js
@@ -1,1 +1,9 @@
 console.log("function.js loaded");
+function greeting() {
+    console.log("word in fuction.js");
+}
+greeting();
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(6, 9));
